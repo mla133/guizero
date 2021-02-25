53877c5994b47317e8fe18913d91d7918b26d2a4
Matt Allen <matthew.allen@technipfmc.com>
2021-02-25 14:27:52 -0500

    Adding in some PNG files for pictures, a button example.

--- wanted.py
+++ wanted.py
@@ -1,8 +1,12 @@
-from guizero import App, Text
+from guizero import App, Text, Picture
 
 app = App("Wanted!")
 app.bg = "#FBFBD0"
 
 wanted_text = Text(app, "WANTED")
+wanted_text.text_size = 50
+wanted_text.text_font = "Courier"
+
+buddy = Picture(app, image="pearl.png")
 app.display()
 
